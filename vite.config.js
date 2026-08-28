add30cb3be43d13d705c37c9aa4ddd891a4d1f1a
GOURAV DUTTA <25051307@kiit.ac.in>
2026-08-28 20:09:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,9 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0',
+
     allowedHosts: ['vatavarnam-1.onrender.com'],
-  },
-});
-  server: {
+
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5000',
