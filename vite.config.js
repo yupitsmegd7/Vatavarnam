7c7fafb5c23050e3f9550fa269226c0a71dea710
GOURAV DUTTA <25051307@kiit.ac.in>
2026-08-28 20:04:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['vatavarnam-1.onrender.com'],
+  },
+});
 <<<<<<< HEAD
 =======
   server: {
